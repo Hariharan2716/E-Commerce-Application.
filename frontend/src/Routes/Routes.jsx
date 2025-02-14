f911c78d1b6db667c8d1d3254587a0a74f7e0d31
Hariharan2716 <hariharan.m2716@gmail.com>
2025-02-14 17:42:41 +0530

    added milestone5

---
+++ frontend/src/Routes/Routes.jsx
@@ -0,0 +1,7 @@
+
+import Login from "../pages/Login.jsx";
+import Signup from "../pages/Signup.jsx";
+import Home from "../pages/Home.jsx"
+import CreateProduct from "../pages/createProduct.jsx";
+import MyProducts from "../pages/myProducts.jsx";
+export { Login,Signup,Home, CreateProduct, MyProducts}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,20 @@
-const { plugin } = require("postcss");
-
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
-
+    fontFamily: {
+      Roboto: ["Roboto", "sans-serif"],
+      Poppins: ['Poppins', "sans-serif"],
+    },
+    extend: { screens: {
+      "1000px": "1050px",
+      "1100px": "1110px",
+      "800px": "800px",
+      "1300px": "1300px",
+      "400px":"400px"
+    },
+},
   },
   plugins: [],
-};
+}
